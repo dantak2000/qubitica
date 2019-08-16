2d3d8de7d130e3cf3b4e0a270eb4d34a4b4c9e63
dantak2000 <36772064+dantak2000@users.noreply.github.com>
2019-08-16 12:56:24 +0200

    Update orderbook-hitbtc_com.php

--- exchange-API/orderbooks/orderbook-hitbtc_com.php
+++ exchange-API/orderbooks/orderbook-hitbtc_com.php
@@ -4,9 +4,6 @@ function orderbook_hitbtc_com($exchange,$symbol,$symbol_orig,$marketid,$quotecur
 {
 $orderbook=array();
 
-date_default_timezone_set('Europe/Berlin');
-$date=date("Y-m-d H:i:s");
-
 $conn = new mysqli("localhost", "exchus1", "$PWD1", "exchange1");
 if ($conn->connect_errno) {echo"error";
 return "Failed to connect to MySQL: (" . $mysqli->connect_errno . ") " . $mysqli->connect_error;
